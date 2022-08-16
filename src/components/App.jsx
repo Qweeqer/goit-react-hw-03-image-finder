960b2eea48bc7952e7b0234ae2bb83d726ebfcbd
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-16 18:11:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,27 @@
-import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import initialContacts from '../contacts.json';
-import { ContactForm } from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import './App.module.css';
+// import { Searchbar } from 'components/Searchbar/Searchbar';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import { App } from 'components/App.styled';
 
-export class App extends Component {
+export class ImageFinder extends React.Component {
   state = {
-    contacts: initialContacts,
-    filter: '',
+    imageSearch: '',
   };
 
-  formSubmit = ({ name, number }) => {
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    this.state.contacts.some(
-      i =>
-        (i.name.toLowerCase() === contact.name.toLowerCase() &&
-          i.number === contact.number) ||
-        i.number === contact.number
-    )
-      ? alert(`${name} is already in contacts`)
-      : this.setState(({ contacts }) => ({
-          contacts: [contact, ...contacts],
-        }));
+  hadleFornSubmit = imageSearch => {
+    this.setState({ imageSearch });
   };
-
-  changeFilterInput = e => {
-    this.setState({ filter: e.target.value });
-  };
-
-  findContacts = () => {
-    const { filter, contacts } = this.state;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  componentDidMount() {
-    // console.log(JSON.parse(localStorage.getItem('contacts')));
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts !== null) {
-      this.setState({ contacts: contacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // console.log('prevProps', prevProps);
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
 
   render() {
     return (
-      <section>
-        <div>
-          <h1>Phonebook</h1>
-          <ContactForm onSubmit={this.formSubmit} />
-          <h2>Contacts</h2>
-          <Filter
-            filter={this.state.filter}
-            changeFilterInput={this.changeFilterInput}
-          />
-          <ContactList
-            contacts={this.findContacts()}
-            deleteContact={this.deleteContact}
-          />
-        </div>
-      </section>
+      <App>
+        {/* <Searchbar onSubmit={this.hadleFornSubmit} /> */}
+        <ImageGallery imageSearch={this.state.imageSearch} />
+        <ToastContainer autoclose={3000} />
+      </App>
     );
   }
 }
