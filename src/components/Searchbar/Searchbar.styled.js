f8213fdaa9699fd45466ff2f0a73f314ad5b5aef
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-08-18 20:15:35 +0300

    Update Searchbar.styled.js
    opacity: 0.6> 0.9;

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -44,7 +44,7 @@ export const SearchButton = styled.button`
   font-style: normal;
   font-weight: 500;
 
-  opacity: 0.6;
+  opacity: 0.9;
   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
   cursor: pointer;
   outline: none;
